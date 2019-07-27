1652a9e91c20a4edf0e7577fe2fd5cdc31ed083c
Anton McConville <anton@hickory.ca>
2019-07-27 17:30:07 -0400

    Integrating data part 1

---
+++ public/javascript/index.js
@@ -0,0 +1,75 @@
+var albums;
+var insights = [];
+
+function dataReader() {
+  console.log(this.responseText);
+
+  var coldplaydata = JSON.parse(this.responseText);
+
+  albums = coldplaydata.sort((a, b) => parseFloat(a.order) - parseFloat(b.order));
+
+  var albumpicker = document.getElementById('albumpicker');
+
+  albums.forEach(function(record) {
+    var option = document.createElement('option');
+    option.value = record.key;
+    option.innerHTML = record.title;
+    albumpicker.appendChild(option);
+    console.log(record.title);
+  })
+
+  albumpicker.onchange = function(event) {
+    console.log(event.target.value);
+    console.log(insights[event.target.value]);
+
+    var src = "./images/svg/" + event.target.value + ".svg"
+    var chris = document.getElementById('chris');
+    chris.src = src;
+    initializeTraitPickers(event.target.value);
+  }
+
+  initializeTraitPickers(albumpicker.value);
+}
+
+function initializeTraitPickers(personalityid){
+  var personality = insights[personalityid].personality;
+
+  var selectid = 'select';
+  var selectcount = 0;
+
+  personality.forEach(function(trait){
+    var traitselect = document.getElementById(selectid + selectcount);
+
+    traitselect.innerHTML = "";
+
+    var newoption = document.createElement('option');
+    newoption.innerHTML = trait.name;
+    newoption.type = 'parent';
+    newoption.value = trait.name;
+    traitselect.appendChild(newoption);
+
+    selectcount++;
+  })
+}
+
+function insightsReader() {
+  console.log(this.responseText);
+  var insightsdata = JSON.parse(this.responseText);
+
+  insightsdata.forEach(function(insight) {
+    insights[insight.album] = insight;
+  })
+}
+
+
+
+var dataRequest = new XMLHttpRequest();
+dataRequest.addEventListener("load", insightsReader);
+dataRequest.open("GET", "./data/coldplay.json");
+dataRequest.send();
+
+
+var dataRequest = new XMLHttpRequest();
+dataRequest.addEventListener("load", dataReader);
+dataRequest.open("GET", "./data/timeline.json");
+dataRequest.send();
