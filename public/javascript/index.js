2ca785185feeeec1a343226ce045e0404e4f7047
Anton McConville <anton@hickory.ca>
2019-07-27 20:23:21 -0400

    lots of data plumbed in now

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,23 @@
 var albums;
 var insights = [];
 
+var chris = document.getElementById('chris');
+
+function changeSelect(reference) {
+  console.log(reference);
+
+  var selector = document.getElementById('select' + reference);
+  var percentile = selector[selector.selectedIndex].value;
+
+  var datalabel = document.getElementById('data' + reference);
+  datalabel.innerHTML = percentile + '%';
+  var whitespace = document.getElementById('whitespace' + reference);
+  var gap = 100 - percentile;
+  whitespace.style.height = gap + '%';
+  var filled = document.getElementById('filled' + reference);
+  filled.style.height = percentile + '%';
+}
+
 function dataReader() {
   console.log(this.responseText);
 
@@ -22,45 +39,57 @@ function dataReader() {
     console.log(event.target.value);
     console.log(insights[event.target.value]);
 
-    var src = "./images/svg/" + event.target.value + ".svg"
-    var chris = document.getElementById('chris');
-    chris.src = src;
+
+    chris.style.opacity = 0;
     initializeTraitPickers(event.target.value);
+    // chris.style.opacity = 1;
   }
 
   initializeTraitPickers(albumpicker.value);
 }
 
-function initializeTraitPickers(personalityid){
+function addOption(trait, selectcount, traitselect) {
+  var percentile = Math.round(trait.percentile * 100);
+
+  var newoption = document.createElement('option');
+  newoption.innerHTML = trait.name;
+  newoption.type = 'parent';
+  newoption.value = percentile;
+  traitselect.appendChild(newoption);
+
+  var datalabel = document.getElementById('data' + selectcount);
+  datalabel.innerHTML = percentile + '%';
+  var whitespace = document.getElementById('whitespace' + selectcount);
+  var gap = 100 - percentile;
+  whitespace.style.height = gap + '%';
+  var filled = document.getElementById('filled' + selectcount);
+  filled.style.height = percentile + '%';
+}
+
+function initializeTraitPickers(personalityid) {
   var personality = insights[personalityid].personality;
 
   var selectid = 'select';
   var selectcount = 0;
 
-  personality.forEach(function(trait){
+  personality.forEach(function(trait) {
     var traitselect = document.getElementById(selectid + selectcount);
     traitselect.innerHTML = "";
+    addOption(trait, selectcount, traitselect);
 
-    var percentile = Math.round(trait.percentile * 100);
-
-    // Math.round(parseInt(trait.percentile)*100)
-
-    var newoption = document.createElement('option');
-    newoption.innerHTML = trait.name;
-    newoption.type = 'parent';
-    newoption.value = percentile;
-    // newoption.value = ;
-    traitselect.appendChild(newoption);
-
-    var datalabel = document.getElementById('data' + selectcount);
-    datalabel.innerHTML = percentile + '%';
-
-
-
-
+    trait.children.forEach(function(childtrait) {
+      addOption(childtrait, selectcount, traitselect);
+    })
 
     selectcount++;
   })
+
+  var src = "./images/svg/" + personalityid + ".svg"
+
+  chris.src = src;
+  chris.style.opacity = 1;
+
+
 }
 
 function insightsReader() {
