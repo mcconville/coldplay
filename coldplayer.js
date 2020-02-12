edcd13abae86e2d4a26dbc8f8c165bb76990e252
Anton McConville <anton@hickory.ca>
2020-02-12 15:50:28 -0500

    Update coldplayer.js

--- coldplayer.js
+++ coldplayer.js
@@ -1,14 +1,34 @@
+/**
+ * Copyright 2015-2020 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var lineReader = require('line-reader');
 var fs = require('fs');
-var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
-
 var config = require('./config/credentials.json');
+const PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const personality_insights = new PersonalityInsightsV3({
+  version: '2019-10-13',
+  authenticator: new IamAuthenticator({
+    apikey: config.personalityInsights.key,
+  }),
+  url: config.personalityInsights.url,
+});
 
 var pi = config.personalityInsights;
-
-var personality_insights = new PersonalityInsightsV3({"username": pi.username, "password": pi.password, version_date: '2016-10-19'});
-
-var fs = require('fs');
 
 var albums=[
     'parachutes',
@@ -28,25 +48,31 @@ albums.forEach( function(album){
       if (err) throw err;
       console.log(data);
 
-      personality_insights.profile({
-        text: data,
-        consumption_preferences: true
-      }, function(err, response) {
-        if (err) {
-          console.log('error:', err);
-        } else {
+      const profileParams = {
+        // Get the content from the JSON file.
+        content: data,
+        contentType: 'text/plain',
+        consumptionPreferences: true,
+        rawScores: true,
+      };
 
-          console.log(album);
-          console.log( response)
-          response.album = album;
-          results.push(response);
+      personality_insights.profile(profileParams)
+    .then(response => {
 
-          fs.writeFile('coldplay.json', JSON.stringify(results), function(err) {
-            if (err) {
-              return console.log(err);
-            }
-          })
-        }
-      });
+        // console.log(JSON.stringify(profile, null, 2));
+        console.log(album);
+        console.log( response)
+        response.album = album;
+        results.push(response);
+
+        fs.writeFile('coldplay.json', JSON.stringify(results), function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        })
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
   });
 });
